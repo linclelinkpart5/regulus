9ab3d1f8c94d992af067745a2a391f0533f02e20
Mark LeMoine <thatsgobbles@gmail.com>
2019-08-14 20:22:41 -0700

    Created `ms_to_samples` in `util`

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
-use crate::constants::DEN_THRESHOLD;
-use crate::constants::MAX_CHANNELS;
+
+const DEN_THRESHOLD: f64 = 1.0e-15;
 
 pub struct Util;
 
@@ -18,5 +18,45 @@ impl Util {
     pub fn den(x: f64) -> f64 {
         if x.abs() < DEN_THRESHOLD { 0.0 }
         else { x }
+    }
+
+    /// Using a sample rate, calculates the number of samples in a given number of milliseconds.
+    pub fn ms_to_samples(ms: u64, sample_rate: u32, round_nearest: bool) -> u64 {
+        let numerator = ms * sample_rate as u64;
+
+        let result = numerator / 1000;
+
+        if round_nearest && numerator % 1000 >= 500 { result + 1 }
+        else { result }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn util_ms_to_samples() {
+        let inputs_and_expected = vec![
+            ((100, 44100, false), 4410),
+            ((100, 44100, true), 4410),
+            ((100, 44123, false), 4412),
+            ((100, 44123, true), 4412),
+            ((1, 44100, false), 44),
+            ((1, 44100, true), 44),
+            ((1, 44600, false), 44),
+            ((1, 44600, true), 45),
+            ((1, 44500, false), 44),
+            ((1, 44500, true), 45),
+            ((1, 44499, false), 44),
+            ((1, 44499, true), 44),
+        ];
+
+        for (inputs, expected) in inputs_and_expected {
+            let (ms, sample_rate, round_nearest) = inputs;
+            let produced = Util::ms_to_samples(ms, sample_rate, round_nearest);
+
+            assert_eq!(expected, produced)
+        }
     }
 }
