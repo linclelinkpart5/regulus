68fac79376b7ae61f5dc496d9d24f5cb67241791
Mark LeMoine <thatsgobbles@gmail.com>
2020-03-06 13:30:46 -0800

    Adding `sample_mean_sq`/`sample_root_mean_sq` to `util`

--- src/util.rs
+++ src/util.rs
@@ -22,7 +22,49 @@ impl Util {
         else { x }
     }
 
-    /// Using a sample rate, calculates the number of samples in a given number of milliseconds.
+    /// Calculates the mean square of an iterable of samples.
+    pub fn sample_mean_sq<I>(samples: I) -> [f64; MAX_CHANNELS]
+    where
+        I: IntoIterator<Item = [f64; MAX_CHANNELS]>
+    {
+        let mut count = 0usize;
+        let mut summed = [0.0f64; MAX_CHANNELS];
+        let mut averaged = [0.0f64; MAX_CHANNELS];
+
+        for sample in samples {
+            // Keep track of the number of samples seen.
+            count += 1;
+
+            // For each channel, square the sample's value, and add it to the running total.
+            for ch in 0..MAX_CHANNELS {
+                summed[ch] += sample[ch] * sample[ch];
+            }
+        }
+
+        if count > 0 {
+            for ch in 0..MAX_CHANNELS {
+                averaged[ch] = summed[ch] / count as f64;
+            }
+        }
+
+        averaged
+    }
+
+    /// Calculates the root mean square of an iterable of samples.
+    pub fn sample_root_mean_sq<I>(samples: I) -> [f64; MAX_CHANNELS]
+    where
+        I: IntoIterator<Item = [f64; MAX_CHANNELS]>
+    {
+        let mean_sqs = Self::sample_mean_sq(samples);
+        let mut root_mean_sqs = [0.0f64; MAX_CHANNELS];
+        for ch in 0..MAX_CHANNELS {
+            root_mean_sqs[ch] = mean_sqs[ch].sqrt();
+        }
+
+        root_mean_sqs
+    }
+
+    /// Calculates the number of samples in a given number of milliseconds with respect to a sample rate.
     pub fn ms_to_samples(ms: u64, sample_rate: u32) -> u64 {
         let num = ms * sample_rate as u64;
 
