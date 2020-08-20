65b7fee8d45e1710a95ef207330e7d41b976b828
Mark LeMoine <linclelinkpart5@gmail.com>
2020-08-20 01:31:24 -0700

    Renaming `Params` to `Coefficients`, and reintroducing filter params as `Parameters`

--- src/biquad.rs
+++ src/biquad.rs
@@ -12,18 +12,20 @@ enum Kind {
 /// Coefficients for a biquad digital filter at a particular sample rate.
 /// It is assumed that the `a0` coefficient is always normalized to 1.0, and thus not included here.
 #[derive(Copy, Clone, Debug, PartialEq)]
-struct Params {
-    a1: f64,
-    a2: f64,
+struct Coefficients {
+    // Numerator coefficients.
     b0: f64,
     b1: f64,
     b2: f64,
+
+    // Denominator coefficients, a0 is implied/assumed to be normalized to 1.0.
+    a1: f64,
+    a2: f64,
 }
 
-impl Params {
+impl Coefficients {
+    // https://hydrogenaud.io/index.php?topic=76394.0
     pub fn new(kind: Kind, sample_rate: u32) -> Self {
-        let g = 3.999843853973347;
-
         let (f0, q) =
             match kind {
                 Kind::A => (1681.974450955533, 0.7071752369554196),
@@ -40,7 +42,9 @@ impl Params {
         let (b0, b1, b2) =
             match kind {
                 Kind::A => {
-                    let vh = 10.0f64.powf(g / 20.0);
+                    let height = 3.999843853973347;
+
+                    let vh = 10.0f64.powf(height / 20.0);
                     let vb = vh.powf(0.4996667741545416);
 
                     let b0 = (vh + vb * k / q + k * k) / a0;
@@ -58,7 +62,7 @@ impl Params {
 }
 
 #[cfg(test)]
-impl AbsDiffEq for Params {
+impl AbsDiffEq for Coefficients {
     type Epsilon = f64;
 
     fn default_epsilon() -> Self::Epsilon {
@@ -74,9 +78,53 @@ impl AbsDiffEq for Params {
     }
 }
 
+/// Filter parameters, which determine a particular frequency response for a
+/// given sampling rate.
+struct Parameters {
+    // Omega, equal to `tan(PI * fc / fs)`.
+    k: f64,
+
+    /// Q factor, used as a quality factor in biquad filters.
+    q: f64,
+
+    // Band-pass gain (@ target frequency `fc`).
+    vb: f64,
+
+    // Low-pass gain (@ DC).
+    vl: f64,
+
+    // High-pass gain (@ Nyquist frequency).
+    vh: f64,
+}
+
+impl Parameters {
+    fn from_coefficients(co: &Coefficients) -> Self {
+        let x11 = co.a1 - 2.0;
+        let x12 = co.a1;
+        let x1 = -co.a1 - 2.0;
+
+        let x21 = co.a2 - 1.0;
+        let x22 = co.a2 + 1.0;
+        let x2 = -co.a2 + 1.0;
+
+        let dx = (x22 * x11) - (x12 * x21);
+        let k_sq = ((x22 * x1) - (x12 * x2))/dx;
+        let k_by_q = ((x11 * x2) - (x21 * x1))/dx;
+        let a0 = 1.0 + k_by_q + k_sq;
+
+        let k = k_sq.sqrt();
+        let q = k / k_by_q;
+        let vb = 0.50 * a0 * (co.b0 - co.b2) / k_by_q;
+        let vl = 0.25 * a0 * (co.b0 + co.b1 + co.b2) / k_sq;
+        let vh = 0.25 * a0 * (co.b0 - co.b1 + co.b2);
+
+        Self { k, q, vb, vl, vh, }
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
 struct Filter {
-    ps: Params,
+    coeff: Coefficients,
     s1: [f64; MAX_CHANNELS],
     s2: [f64; MAX_CHANNELS],
 }
@@ -84,7 +132,7 @@ struct Filter {
 impl Filter {
     fn new(kind: Kind, sample_rate: u32) -> Self {
         Self {
-            ps: Params::new(kind, sample_rate),
+            coeff: Coefficients::new(kind, sample_rate),
             s1: [0.0; MAX_CHANNELS],
             s2: [0.0; MAX_CHANNELS],
         }
@@ -94,10 +142,11 @@ impl Filter {
         let mut output = [0.0f64; MAX_CHANNELS];
 
         // https://www.earlevel.com/main/2012/11/26/biquad-c-source-code/
+        // https://github.com/korken89/biquad-rs/blob/master/src/lib.rs
         for ch in 0..MAX_CHANNELS {
-            let out = self.s1[ch] + self.ps.b0 * input[ch];
-            self.s1[ch] = self.s2[ch] + self.ps.b1 * input[ch] - self.ps.a1 * out;
-            self.s2[ch] = self.ps.b2 * input[ch] - self.ps.a2 * out;
+            let out = self.s1[ch] + self.coeff.b0 * input[ch];
+            self.s1[ch] = self.s2[ch] + self.coeff.b1 * input[ch] - self.coeff.a1 * out;
+            self.s2[ch] = self.coeff.b2 * input[ch] - self.coeff.a2 * out;
 
             output[ch] = out;
         }
@@ -107,8 +156,9 @@ impl Filter {
 }
 
 /// The initial two-pass "K"-filter as described by the ITU-R BS.1770-4 spec.
-/// The first pass is a shelving filter, which accounts for the acoustic effects of the listener's (spherical) head.
-/// The second pass is a simple high pass filter.
+/// The first pass is a high shelf boost filter, which accounts for the acoustic
+/// effects of the listener's head, assumed to be roughly spherical. The second
+/// pass is a simple high pass filter.
 #[derive(Copy, Clone, Debug)]
 struct FilterChain {
     pass_a: Filter,
@@ -169,62 +219,62 @@ mod tests {
 
     #[test]
     fn params_new() {
-        let expected = Params {
+        let expected = Coefficients {
             a1: -1.6906592931824103,
             a2: 0.7324807742158501,
             b0: 1.5351248595869702,
             b1: -2.6916961894063807,
             b2: 1.19839281085285,
         };
-        let produced = Params::new(Kind::A, 48000);
+        let produced = Coefficients::new(Kind::A, 48000);
 
         println!("{:?}, {:?}", expected, produced);
         assert_abs_diff_eq!(expected, produced);
 
-        let expected = Params {
+        let expected = Coefficients {
             a1: -1.6636551132560204,
             a2: 0.7125954280732254,
             b0: 1.5308412300503478,
             b1: -2.6509799951547297,
             b2: 1.169079079921587,
         };
-        let produced = Params::new(Kind::A, 44100);
+        let produced = Coefficients::new(Kind::A, 44100);
 
         println!("{:?}, {:?}", expected, produced);
         assert_abs_diff_eq!(expected, produced);
 
-        let expected = Params {
+        let expected = Coefficients {
             a1: -0.2933807824149212,
             a2: 0.18687510604540827,
             b0: 1.3216235689299776,
             b1: -0.7262554913156911,
             b2: 0.2981262460162007,
         };
-        let produced = Params::new(Kind::A, 8000);
+        let produced = Coefficients::new(Kind::A, 8000);
 
         println!("{:?}, {:?}", expected, produced);
         assert_abs_diff_eq!(expected, produced);
 
-        let expected = Params {
+        let expected = Coefficients {
             a1: -1.9222022306074886,
             a2: 0.925117735116826,
             b0: 1.5722272150912788,
             b1: -3.0472830515615508,
             b2: 1.4779713409796091,
         };
-        let produced = Params::new(Kind::A, 192000);
+        let produced = Coefficients::new(Kind::A, 192000);
 
         println!("{:?}, {:?}", expected, produced);
         assert_abs_diff_eq!(expected, produced);
 
-        let expected = Params {
+        let expected = Coefficients {
             a1: -1.99004745483398,
             a2:  0.99007225036621,
             b0:  1.00000000000000,
             b1: -2.00000000000000,
             b2:  1.00000000000000,
         };
-        let produced = Params::new(Kind::B, 48000);
+        let produced = Coefficients::new(Kind::B, 48000);
 
         println!("{:?}, {:?}", expected, produced);
         assert_abs_diff_eq!(expected, produced);
@@ -235,8 +285,8 @@ mod tests {
         let filter = Filter::new(Kind::A, 48000);
 
         assert_abs_diff_eq!(
-            filter.ps,
-            Params {
+            filter.coeff,
+            Coefficients {
                 a1: -1.6906592931824103,
                 a2: 0.7324807742158501,
                 b0: 1.5351248595869702,
