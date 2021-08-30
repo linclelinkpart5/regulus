bf9746053393536270f2d7c791c963467182afc7
Mark LeMoine <linclelinkpart5@gmail.com>
2021-08-30 01:35:49 -0700

    Re-enable `pipeline` module, add `PipelineBuilder`, use `HashMap`/`HashSet` for storage in `pipeline`

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,59 +1,98 @@
-// use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-// use sampara::{Frame, Calculator};
+use sampara::{Frame, Calculator};
 
-// use crate::filter::KWeightFilter;
-// use crate::gated_loudness::{Gating, GatedLoudness};
+use crate::filter::KWeightFilter;
+use crate::gated_loudness::{Gating, GatedLoudness, MOMENTARY_GATING, SHORTTERM_GATING};
 
-// pub struct PipelineBuilder
+pub struct PipelineBuilder<F, const N: usize>
+where
+    F: Frame<N, Sample = f64>,
+{
+    sample_rate: u32,
+    g_weights: F,
+    gatings: HashSet<Gating>,
+}
 
-// pub struct Pipeline<F, const N: usize>
-// where
-//     F: Frame<N, Sample = f64>,
-// {
-//     k_filter: KWeightFilter<F, N>,
-//     gated_loudness_specs: HashMap<Gating, GatedLoudness<F, N>>,
-// }
+impl<F, const N: usize> PipelineBuilder<F, N>
+where
+    F: Frame<N, Sample = f64>,
+{
+    pub fn new(sample_rate: u32, g_weights: F) -> Self {
+        Self {
+            sample_rate,
+            g_weights,
+            gatings: HashSet::new(),
+        }
+    }
 
-// impl<F, const N: usize> Pipeline<F, N>
-// where
-//     F: Frame<N, Sample = f64>,
-// {
-//     pub fn new(sample_rate: u32, g_weights: F) -> Self {
-//         let k_filter = KWeightFilter::new(sample_rate);
-//         let gated_loudness_specs = HashMap::new();
+    #[inline]
+    pub fn custom_gating(&mut self, gating: Gating) -> &mut Self {
+        self.gatings.insert(gating);
+        self
+    }
 
-//         Self {
-//             k_filter,
-//             gated_loudness_specs,
-//         }
-//     }
+    pub fn momentary_gating(&mut self) -> &mut Self {
+        self.custom_gating(MOMENTARY_GATING)
+    }
 
-//     pub fn is_noop(&self) -> bool {
-//         self.gated_loudness_specs.is_empty()
-//     }
-// }
+    pub fn shortterm_gating(&mut self) -> &mut Self {
+        self.custom_gating(SHORTTERM_GATING)
+    }
 
-// impl<F, const N: usize> Calculator for Pipeline<F, N>
-// where
-//     F: Frame<N, Sample = f64>,
-// {
-//     type Input = F;
-//     type Output = [Option<f64>; P];
+    pub fn build(&mut self) -> Pipeline<F, N> {
+        let sample_rate = self.sample_rate;
+        let g_weights = self.g_weights;
 
-//     fn push(&mut self, input: Self::Input) {
-//         let filtered_frame = self.k_filter.process(input);
+        let k_filter = KWeightFilter::new(sample_rate);
+        let gated_loudness_map = self.gatings.drain()
+            .map(|g| (g, GatedLoudness::custom(g, sample_rate, g_weights)))
+            .collect::<HashMap<_, _>>();
 
-//         let opt_powers = self.gated_power_pipelines.each_mut().map(|gpp| {
-//             gpp.process(filtered_frame)
-//         });
+        Pipeline {
+            k_filter,
+            gated_loudness_map,
+        }
+    }
+}
 
-//         self.loudness_pipelines.each_mut().zip(opt_powers).map(|(lp, opt_power)| {
-//             opt_power.map(|p| lp.push(p))
-//         });
-//     }
+pub struct Pipeline<F, const N: usize>
+where
+    F: Frame<N, Sample = f64>,
+{
+    k_filter: KWeightFilter<F, N>,
+    gated_loudness_map: HashMap<Gating, GatedLoudness<F, N>>,
+}
 
-//     fn calculate(self) -> Self::Output {
-//         self.loudness_pipelines.map(|lp| lp.calculate())
-//     }
-// }
+impl<F, const N: usize> Pipeline<F, N>
+where
+    F: Frame<N, Sample = f64>,
+{
+    pub fn is_noop(&self) -> bool {
+        self.gated_loudness_map.is_empty()
+    }
+}
+
+impl<F, const N: usize> Calculator for Pipeline<F, N>
+where
+    F: Frame<N, Sample = f64>,
+{
+    type Input = F;
+    type Output = HashMap<Gating, Option<f64>>;
+
+    fn push(&mut self, input: Self::Input) {
+        let filtered_frame = self.k_filter.process(input);
+
+        for gated_loudness in self.gated_loudness_map.values_mut() {
+            gated_loudness.push(filtered_frame);
+        }
+    }
+
+    fn calculate(self) -> Self::Output {
+        self.gated_loudness_map.into_iter()
+            .map(|(gating, gated_loudness)| {
+                (gating, gated_loudness.calculate())
+            })
+            .collect()
+    }
+}
