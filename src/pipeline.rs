c49f1ee29b2c43f5b2a8af796782ff785296d781
Mark LeMoine <linclelinkpart5@gmail.com>
2021-09-12 00:48:33 -0700

    Delete old `Pipeline` class and helpers, rename `LayeredPipeline` to `Pipeline`, update `Output` to support averages/maxima

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -6,53 +6,8 @@ use crate::filter::KWeightFilter;
 use crate::gated_loudness::{Gating, GatedLoudness};
 
 pub struct Output {
-    momentary_mean: Option<f64>,
-    shortterm_mean: Option<f64>,
-    custom_gating_means: HashMap<Gating, Option<f64>>
-}
-
-pub struct Config<F, const N: usize>
-where
-    F: Frame<N, Sample = f64>,
-{
-    pub sample_rate: u32,
-    pub g_weights: F
-}
-
-enum Upstream<F, const N: usize>
-where
-    F: Frame<N, Sample = f64>,
-{
-    Parent(Box<Pipeline<F, N>>),
-    Source(Config<F, N>),
-}
-
-impl<F, const N: usize> Upstream<F, N>
-where
-    F: Frame<N, Sample = f64>,
-{
-    #[inline(always)]
-    fn config(&self) -> &Config<F, N> {
-        match self {
-            Self::Parent(parent_pipeline) => parent_pipeline.upstream.config(),
-            Self::Source(config) => config,
-        }
-    }
-
-    #[inline(always)]
-    fn push(&mut self, input: F) {
-        match self {
-            Self::Parent(parent_pipeline) => parent_pipeline.push(input),
-            Self::Source(..) => {},
-        }
-    }
-
-    fn into_parent(self) -> Option<Pipeline<F, N>> {
-        match self {
-            Self::Parent(parent_pipeline) => Some(Box::into_inner(parent_pipeline)),
-            Self::Source(..) => None,
-        }
-    }
+    averages: HashMap<Gating, Option<f64>>,
+    maximas: HashMap<Gating, Option<f64>>,
 }
 
 pub struct PipelineLayer<F, const N: usize>
@@ -60,9 +15,8 @@ where
     F: Frame<N, Sample = f64>,
 {
     k_filter: KWeightFilter<F, N>,
-    momentary_gl: Option<GatedLoudness<F, N>>,
-    shortterm_gl: Option<GatedLoudness<F, N>>,
-    custom_gl_map: HashMap<Gating, GatedLoudness<F, N>>,
+    gl_average_map: HashMap<Gating, GatedLoudness<F, N>>,
+    gl_maxima_map: HashMap<Gating, GatedLoudness<F, N>>,
 }
 
 impl<F, const N: usize> PipelineLayer<F, N>
@@ -70,7 +24,7 @@ where
     F: Frame<N, Sample = f64>,
 {
     pub fn is_noop(&self) -> bool {
-        self.momentary_gl.is_none() && self.shortterm_gl.is_none() && self.custom_gl_map.is_empty()
+        self.gl_average_map.is_empty() && self.gl_maxima_map.is_empty()
     }
 
     pub fn feed<I>(&mut self, frames: I)
@@ -85,23 +39,31 @@ where
     pub fn push(&mut self, input: F) {
         let filtered_frame = self.k_filter.process(input);
 
-        self.momentary_gl.as_mut().map(|gl| gl.push(filtered_frame));
-        self.shortterm_gl.as_mut().map(|gl| gl.push(filtered_frame));
+        for gated_loudness in self.gl_average_map.values_mut() {
+            gated_loudness.push(filtered_frame);
+        }
 
-        for gated_loudness in self.custom_gl_map.values_mut() {
+        for gated_loudness in self.gl_maxima_map.values_mut() {
             gated_loudness.push(filtered_frame);
         }
     }
 
     pub fn calculate(self) -> Output {
+        let averages = self.gl_average_map.into_iter()
+            .map(|(gating, gl)| {
+                (gating, gl.calculate())
+            })
+            .collect();
+
+        let maximas = self.gl_maxima_map.into_iter()
+            .map(|(gating, gl)| {
+                (gating, gl.calculate())
+            })
+            .collect();
+
         Output {
-            momentary_mean: self.momentary_gl.and_then(|gl| gl.calculate()),
-            shortterm_mean: self.shortterm_gl.and_then(|gl| gl.calculate()),
-            custom_gating_means: self.custom_gl_map.into_iter()
-                .map(|(gating, gl)| {
-                    (gating, gl.calculate())
-                })
-                .collect(),
+            averages,
+            maximas,
         }
     }
 }
@@ -122,19 +84,16 @@ where
     }
 }
 
-pub struct LayeredPipeline<F, const N: usize>
+pub struct Pipeline<F, const N: usize>
 where
     F: Frame<N, Sample = f64>,
 {
     pub sample_rate: u32,
     pub g_weights: F,
 
-    // Flags for calculating preset/common gatings.
-    calc_momentary: bool,
-    calc_shortterm: bool,
-
-    // Custom gatings to calculate, usually will be empty.
-    custom_gatings: HashSet<Gating>,
+    // Gatings to calculate for both averages and maximas.
+    average_gatings: HashSet<Gating>,
+    maxima_gatings: HashSet<Gating>,
 
     root_layer: PipelineLayer<F, N>,
 
@@ -142,7 +101,7 @@ where
     child_layers: Vec<PipelineLayer<F, N>>,
 }
 
-impl<F, const N: usize> LayeredPipeline<F, N>
+impl<F, const N: usize> Pipeline<F, N>
 where
     F: Frame<N, Sample = f64>,
 {
@@ -181,18 +140,20 @@ where
         let g_weights = self.g_weights;
 
         let k_filter = KWeightFilter::new(sample_rate);
-        let momentary_gl = self.calc_momentary.then(|| GatedLoudness::momentary(sample_rate, g_weights));
-        let shortterm_gl = self.calc_shortterm.then(|| GatedLoudness::shortterm(sample_rate, g_weights));
-        let custom_gl_map = self.custom_gatings
+
+        let gl_average_map = self.average_gatings
+            .iter()
+            .map(|g| (*g, GatedLoudness::new(sample_rate, g_weights, *g)))
+            .collect::<HashMap<_, _>>();
+        let gl_maxima_map = self.maxima_gatings
             .iter()
             .map(|g| (*g, GatedLoudness::new(sample_rate, g_weights, *g)))
             .collect::<HashMap<_, _>>();
 
         PipelineLayer {
             k_filter,
-            momentary_gl,
-            shortterm_gl,
-            custom_gl_map,
+            gl_average_map,
+            gl_maxima_map,
         }
     }
 
@@ -211,7 +172,7 @@ where
     }
 }
 
-impl<F, const N: usize> Calculator for LayeredPipeline<F, N>
+impl<F, const N: usize> Calculator for Pipeline<F, N>
 where
     F: Frame<N, Sample = f64>,
 {
@@ -224,168 +185,5 @@ where
 
     fn calculate(self) -> Self::Output {
         self.calculate()
-    }
-}
-
-pub struct Pipeline<F, const N: usize>
-where
-    F: Frame<N, Sample = f64>,
-{
-    upstream: Upstream<F, N>,
-
-    k_filter: KWeightFilter<F, N>,
-    momentary_gl: Option<GatedLoudness<F, N>>,
-    shortterm_gl: Option<GatedLoudness<F, N>>,
-    custom_gl_map: HashMap<Gating, GatedLoudness<F, N>>,
-}
-
-impl<F, const N: usize> Pipeline<F, N>
-where
-    F: Frame<N, Sample = f64>,
-{
-    pub fn is_noop(&self) -> bool {
-        self.momentary_gl.is_none() && self.shortterm_gl.is_none() && self.custom_gl_map.is_empty()
-    }
-
-    pub fn feed<S>(&mut self, signal: S)
-    where
-        S: Signal<N, Frame = F>,
-    {
-        for frame in signal.into_iter() {
-            self.push(frame);
-        }
-    }
-
-    pub fn spawn_child(self) -> Self {
-        let &Config { sample_rate, g_weights } = self.upstream.config();
-
-        let k_filter = KWeightFilter::new(sample_rate);
-        let momentary_gl = self.momentary_gl.is_some().then(|| {
-            GatedLoudness::momentary(sample_rate, g_weights)
-        });
-        let shortterm_gl = self.shortterm_gl.is_some().then(|| {
-            GatedLoudness::shortterm(sample_rate, g_weights)
-        });
-
-        let custom_gl_map = self.custom_gl_map.keys()
-            .copied()
-            .map(|gating| {
-                (gating, GatedLoudness::new(sample_rate, g_weights, gating))
-            })
-            .collect()
-        ;
-
-        Self {
-            k_filter,
-            momentary_gl,
-            shortterm_gl,
-            custom_gl_map,
-
-            upstream: Upstream::Parent(Box::new(self)),
-        }
-    }
-}
-
-impl<F, const N: usize> Calculator for Pipeline<F, N>
-where
-    F: Frame<N, Sample = f64>,
-{
-    type Input = F;
-    type Output = (Output, Option<Pipeline<F, N>>);
-
-    fn push(&mut self, input: Self::Input) {
-        let filtered_frame = self.k_filter.process(input);
-
-        self.momentary_gl.as_mut().map(|gl| gl.push(filtered_frame));
-        self.shortterm_gl.as_mut().map(|gl| gl.push(filtered_frame));
-
-        for gated_loudness in self.custom_gl_map.values_mut() {
-            gated_loudness.push(filtered_frame);
-        }
-
-        // Push the frame upstream as well.
-        self.upstream.push(input);
-    }
-
-    fn calculate(self) -> Self::Output {
-        let output = Output {
-            momentary_mean: self.momentary_gl.and_then(|gl| gl.calculate()),
-            shortterm_mean: self.shortterm_gl.and_then(|gl| gl.calculate()),
-            custom_gating_means: self.custom_gl_map.into_iter()
-                .map(|(gating, gl)| {
-                    (gating, gl.calculate())
-                })
-                .collect(),
-        };
-
-        let parent = self.upstream.into_parent();
-
-        (output, parent)
-    }
-}
-
-pub struct PipelineBuilder<F, const N: usize>
-where
-    F: Frame<N, Sample = f64>,
-{
-    sample_rate: u32,
-    g_weights: F,
-    calc_momentary: bool,
-    calc_shortterm: bool,
-    custom_gatings: HashSet<Gating>,
-}
-
-impl<F, const N: usize> PipelineBuilder<F, N>
-where
-    F: Frame<N, Sample = f64>,
-{
-    pub fn new(sample_rate: u32, g_weights: F) -> Self {
-        Self {
-            sample_rate,
-            g_weights,
-            calc_momentary: false,
-            calc_shortterm: false,
-            custom_gatings: HashSet::new(),
-        }
-    }
-
-    #[inline]
-    pub fn momentary(&mut self) -> &mut Self {
-        self.calc_momentary = true;
-        self
-    }
-
-    #[inline]
-    pub fn shortterm(&mut self) -> &mut Self {
-        self.calc_shortterm = true;
-        self
-    }
-
-    #[inline]
-    pub fn custom(&mut self, gate_len_ms: u64, delta_len_ms: u64) -> &mut Self {
-        self.custom_gatings.insert(Gating::Custom { gate_len_ms, delta_len_ms });
-        self
-    }
-
-    pub fn build(&mut self) -> Pipeline<F, N> {
-        let sample_rate = self.sample_rate;
-        let g_weights = self.g_weights;
-
-        let upstream = Upstream::Source(Config { sample_rate, g_weights });
-        let k_filter = KWeightFilter::new(sample_rate);
-        let momentary_gl = self.calc_momentary.then(|| GatedLoudness::momentary(sample_rate, g_weights));
-        let shortterm_gl = self.calc_shortterm.then(|| GatedLoudness::shortterm(sample_rate, g_weights));
-        let custom_gl_map = self.custom_gatings.drain()
-            .map(|g| (g, GatedLoudness::new(sample_rate, g_weights, g)))
-            .collect::<HashMap<_, _>>();
-
-        Pipeline {
-            upstream,
-
-            k_filter,
-            momentary_gl,
-            shortterm_gl,
-            custom_gl_map,
-        }
     }
 }
